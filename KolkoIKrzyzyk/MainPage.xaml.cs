1c530a3ba35d52f72c07fead034e8904f172ef37
Kubulax <kubulax25@gmail.com>
2024-02-24 21:16:45 +0100

    Dodaj pliki projektów.

---
+++ KolkoIKrzyzyk/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace KolkoIKrzyzyk
+{
+    public partial class MainPage : ContentPage
+    {
+        string currentPlayer = "×";
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        public void DrawSymbol(object sender, EventArgs e)
+        {
+            Button field = sender as Button;
+
+            field.Text = currentPlayer;
+        }
+
+        public void ResetRound(object sender, EventArgs e)
+        {
+            Button field = sender as Button;
+
+            field.Text = currentPlayer;
+        }
+
+        public void ResetGame(object sender, EventArgs e)
+        {
+            Button field = sender as Button;
+
+            field.Text = currentPlayer;
+        }
+    }
+}
