782bdfe743bb67da0f123dc64dd76f75a07e32e9
Kubulax <kubulax25@gmail.com>
2024-02-25 02:03:39 +0100

    idk

--- KolkoIKrzyzyk/MainPage.xaml.cs
+++ KolkoIKrzyzyk/MainPage.xaml.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections;
 using System.Collections.Generic;
 using System.ComponentModel;
 using System.Linq;
@@ -11,6 +12,9 @@ namespace KolkoIKrzyzyk
     public partial class MainPage : ContentPage
     {
         string currentPlayer = "×";
+        bool hasWinner = false;
+        int roundsCounter = 0;
+        
         public MainPage()
         {
             InitializeComponent();
@@ -20,21 +24,163 @@ namespace KolkoIKrzyzyk
         {
             Button field = sender as Button;
 
-            field.Text = currentPlayer;
+            if(String.IsNullOrWhiteSpace(field.Text) && !hasWinner && roundsCounter != 9)
+            {
+                roundsCounter++;
+
+                field.Text = currentPlayer;
+
+                if (currentPlayer == "×")
+                {
+                    lbl_playerIndicator.Text = "Teraz gra: ○";
+                }
+                else
+                {
+                    lbl_playerIndicator.Text = "Teraz gra: ×";
+                }
+
+                CheckForWinner();
+
+                if (currentPlayer == "×")
+                {
+                    currentPlayer = "○";
+                }
+                else
+                {
+                    currentPlayer = "×";
+                }
+            }       
         }
 
-        public void ResetRound(object sender, EventArgs e)
+        private void CheckForWinner()
         {
-            Button field = sender as Button;
+            if(lbl_R0_C0.Text == currentPlayer && lbl_R0_C1.Text == currentPlayer && lbl_R0_C2.Text == currentPlayer)
+            {
+                lbl_R0_C0.TextColor = Color.Blue;
+                lbl_R0_C1.TextColor = Color.Blue;
+                lbl_R0_C2.TextColor = Color.Blue;
 
-            field.Text = currentPlayer;
+                hasWinner = true;
+            }
+            else if (lbl_R1_C0.Text == currentPlayer && lbl_R1_C1.Text == currentPlayer && lbl_R1_C2.Text == currentPlayer)
+            {
+                lbl_R1_C0.TextColor = Color.Blue;
+                lbl_R1_C1.TextColor = Color.Blue;
+                lbl_R1_C2.TextColor = Color.Blue;
+
+                hasWinner = true;
+            }
+            else if (lbl_R2_C0.Text == currentPlayer && lbl_R2_C1.Text == currentPlayer && lbl_R2_C2.Text == currentPlayer)
+            {
+                lbl_R2_C0.TextColor = Color.Blue;
+                lbl_R2_C1.TextColor = Color.Blue;
+                lbl_R2_C2.TextColor = Color.Blue;
+
+                hasWinner = true;
+            }
+
+            else if (lbl_R0_C0.Text == currentPlayer && lbl_R1_C0.Text == currentPlayer && lbl_R2_C0.Text == currentPlayer)
+            {
+                lbl_R0_C0.TextColor = Color.Blue;
+                lbl_R1_C0.TextColor = Color.Blue;
+                lbl_R2_C0.TextColor = Color.Blue;
+
+                hasWinner = true;
+            }
+            else if (lbl_R0_C1.Text == currentPlayer && lbl_R1_C1.Text == currentPlayer && lbl_R2_C1.Text == currentPlayer)
+            {
+                lbl_R0_C1.TextColor = Color.Blue;
+                lbl_R1_C1.TextColor = Color.Blue;
+                lbl_R2_C1.TextColor = Color.Blue;
+
+                hasWinner = true;
+            }
+            else if (lbl_R0_C2.Text == currentPlayer && lbl_R1_C2.Text == currentPlayer && lbl_R2_C2.Text == currentPlayer)
+            {
+                lbl_R0_C2.TextColor = Color.Blue;
+                lbl_R1_C2.TextColor = Color.Blue;
+                lbl_R2_C2.TextColor = Color.Blue;
+
+                hasWinner = true;
+            }
+            
+            else if(lbl_R0_C2.Text == currentPlayer && lbl_R1_C1.Text == currentPlayer && lbl_R2_C0.Text == currentPlayer)
+            {
+                lbl_R0_C2.TextColor = Color.Blue;
+                lbl_R1_C1.TextColor = Color.Blue;
+                lbl_R2_C0.TextColor = Color.Blue;
+
+                hasWinner = true;
+            }
+            else if (lbl_R0_C0.Text == currentPlayer && lbl_R1_C1.Text == currentPlayer && lbl_R2_C2.Text == currentPlayer)
+            {
+                lbl_R0_C0.TextColor = Color.Blue;
+                lbl_R1_C1.TextColor = Color.Blue;
+                lbl_R2_C2.TextColor = Color.Blue;
+
+                hasWinner = true;
+            }
+
+            if(hasWinner && roundsCounter <= 9)
+            {
+                ScoreSystem.AddPoint(currentPlayer);
+                DisplayAlert("Koniec gry", "Wygrywa " + currentPlayer, "OK");
+                lbl_score.Text = ScoreSystem.DrawScoreCounter();
+                lbl_playerIndicator.Text = "Wygrał " + currentPlayer;
+
+                return;
+            }
+
+            if (!hasWinner && roundsCounter == 9)
+            {
+                DisplayAlert("Koniec gry", "Remis", "OK");
+                lbl_playerIndicator.Text = "Remis";
+
+                return;
+            }
+        }
+
+        public void NewRound(object sender, EventArgs e)
+        {
+            ResetRoundStaus();
         }
 
         public void ResetGame(object sender, EventArgs e)
         {
-            Button field = sender as Button;
+            ResetRoundStaus();
 
-            field.Text = currentPlayer;
+            ScoreSystem.ResetScore();
+
+            lbl_score.Text = ScoreSystem.DrawScoreCounter();
+        }
+
+        private void ResetRoundStaus()
+        {
+            currentPlayer = "×";
+            hasWinner = false;
+            roundsCounter = 0;
+
+            lbl_R0_C0.Text = "";
+            lbl_R0_C1.Text = "";
+            lbl_R0_C2.Text = "";
+            lbl_R1_C0.Text = "";
+            lbl_R1_C1.Text = "";
+            lbl_R1_C2.Text = "";
+            lbl_R2_C0.Text = "";
+            lbl_R2_C1.Text = "";
+            lbl_R2_C2.Text = "";
+
+            lbl_R0_C0.TextColor = Color.Black;
+            lbl_R0_C1.TextColor = Color.Black;
+            lbl_R0_C2.TextColor = Color.Black;
+            lbl_R1_C0.TextColor = Color.Black;
+            lbl_R1_C1.TextColor = Color.Black;
+            lbl_R1_C2.TextColor = Color.Black;
+            lbl_R2_C0.TextColor = Color.Black;
+            lbl_R2_C1.TextColor = Color.Black;
+            lbl_R2_C2.TextColor = Color.Black;
+
+            lbl_playerIndicator.Text = "Teraz gra: ×";
         }
     }
 }
---
+++ KolkoIKrzyzyk/ScoreSystem.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace KolkoIKrzyzyk
+{
+    public static class ScoreSystem
+    {
+        private static int[] score = new int[2];
+
+        public static void AddPoint(string player)
+        {
+            if (player == "×")
+            {
+                score[0] += 1;
+            }
+            else if (player == "○")
+            {
+                score[1] += 1;
+            }
+        }
+
+        public static void ResetScore()
+        {
+            score = new int[2];
+        }
+
+        public static string DrawScoreCounter()
+        {
+            return "× " + score[0] + " : " + score[1] + " ○";
+        }
+
+    }
+}
